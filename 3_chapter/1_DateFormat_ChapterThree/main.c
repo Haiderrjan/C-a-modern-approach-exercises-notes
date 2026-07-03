b59aea2d7e822f83754791f9f1f4a82c264bf624
Haider Jan <Haider@DESKTOP-IMIJS5T>
2026-07-03 15:08:38 +0100

    adding all projects and exercises

---
+++ 3_chapter/1_DateFormat_ChapterThree/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int day;
+    int month;
+    int year;
+
+    printf("Enter a date: ");
+    scanf("%d/%d/%d", &day, &month, &year);
+
+    printf("You entered the date %d%d%d", year, month, day);
+
+
+
+
+
+    return 0;
+
+
+
+
+}
