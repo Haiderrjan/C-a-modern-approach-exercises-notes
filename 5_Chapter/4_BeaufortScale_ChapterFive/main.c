f18427ee5e9168d322cf66d21d8f8e0593cc8eaa
Haider Jan <Haider@DESKTOP-IMIJS5T>
2026-07-10 16:09:12 +0100

    completed project 4 chapter 5

--- 5_Chapter/4_BeaufortScale_ChapterFive/main.c
+++ 5_Chapter/4_BeaufortScale_ChapterFive/main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+
+    int windSpeed;
+
+    printf("Enter a wind speed (in knots): ");
+    scanf("%d",&windSpeed);
+
+
+
+    if (windSpeed < 1) {
+        printf("The wind speed is Calm\n");
+    } else if (windSpeed >= 1 && windSpeed <= 3) {
+        printf("The wind speed is light air\n");
+    } else if (windSpeed >= 4 && windSpeed <= 27) {
+        printf("The wind speed is Breeze\n");
+    } else if (windSpeed >= 28 && windSpeed <= 47) {
+        printf("The wind speed is Gale\n");
+    } else if (windSpeed >= 48 && windSpeed <= 63) {
+        printf("The wind speed is Storm\n");
+    } else {
+        printf("the wind speed is Hurricane\n");
+    }
+
     return 0;
 }
