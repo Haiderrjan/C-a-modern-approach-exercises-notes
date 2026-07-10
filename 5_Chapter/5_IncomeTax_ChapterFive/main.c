52d92b4384a0c44ee93578730641a460e5f92d04
Haider Jan <Haider@DESKTOP-IMIJS5T>
2026-07-10 16:57:24 +0100

    my way of completetion which if else was correct however the tax income stuff was consfusing and got the formula wrong

--- 5_Chapter/5_IncomeTax_ChapterFive/main.c
+++ 5_Chapter/5_IncomeTax_ChapterFive/main.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+
+    float income;
+    float tax;
+
+    printf("Enter amount of taxable income:");
+    scanf("%f",&income);
+
+    if (income <= 750) {
+        tax = income * 0.01f;
+    } else if (income <= 2250) {
+        tax = 7.50f + (income * 0.02f);
+    } else if (income <= 3750) {
+        tax = 37.50f + (income * 0.03f);
+    } else if  (income <= 5250){
+        tax = 82.50f + (income * 0.04f);
+
+    } else if (income <= 7000) {
+        tax = 142.50f + (income * 0.05f);
+
+    } else {
+        tax = 230.00f + (income * 0.06f);
+    }
+
+    printf("taxable income: $%.2f",tax);
+
     return 0;
+
 }
