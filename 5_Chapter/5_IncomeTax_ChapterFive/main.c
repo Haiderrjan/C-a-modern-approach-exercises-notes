eceac4bdbdb3ec6dda341e3074f2b28563eda291
Haider Jan <Haider@DESKTOP-IMIJS5T>
2026-07-10 17:06:02 +0100

    completed project 5 in chapter 5 (formula confuse me but i understand the concept of if/else statement and making expressions within them)

--- 5_Chapter/5_IncomeTax_ChapterFive/main.c
+++ 5_Chapter/5_IncomeTax_ChapterFive/main.c
@@ -11,20 +11,20 @@ int main(void) {
     if (income <= 750) {
         tax = income * 0.01f;
     } else if (income <= 2250) {
-        tax = 7.50f + (income * 0.02f);
+        tax = 7.50f + (0.02f * (income - 750.00f));
     } else if (income <= 3750) {
-        tax = 37.50f + (income * 0.03f);
+        tax = 37.50f + (0.03f * (income - 2250.00f));
     } else if  (income <= 5250){
-        tax = 82.50f + (income * 0.04f);
+        tax = 82.50f + (0.04f * (income - 3750.00f));
 
     } else if (income <= 7000) {
-        tax = 142.50f + (income * 0.05f);
+        tax = 142.50f + (0.05f * (income - 5250.00f));
 
     } else {
-        tax = 230.00f + (income * 0.06f);
+        tax = 230.00f + (0.06f * (income - 7000.00f));
     }
 
-    printf("taxable income: $%.2f",tax);
+    printf("taxable income: $%.2f \n",tax);
 
     return 0;
 
