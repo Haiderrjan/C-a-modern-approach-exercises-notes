9803a1c5030cc38057bb2c690c10158e2145ad83
Haider Jan <Haider@DESKTOP-IMIJS5T>
2026-07-17 15:29:18 +0100

    completed this project with success project 7 chapter 5

--- 5_Chapter/7_SmallestToLargest_ChapterFive/main.c
+++ 5_Chapter/7_SmallestToLargest_ChapterFive/main.c
@@ -1,6 +1,44 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+
+    int numOne;
+    int numTwo;
+    int numThree;
+    int numFour;
+
+    int largest;
+    int smallest;
+
+    printf("Enter four integers: ");
+    scanf("%d %d %d %d", &numOne, &numTwo, &numThree, &numFour);
+
+
+    if (numOne > numTwo && numOne > numThree && numOne > numFour) {
+        largest = numOne;
+    } else if (numTwo > numOne && numTwo > numThree && numTwo > numFour) {
+        largest = numTwo;
+
+    } else if (numThree > numOne && numThree > numTwo && numThree > numFour) {
+        largest = numThree;
+    } else {
+        largest = numFour;
+    }
+
+    if (numOne < numTwo && numOne < numThree && numOne < numFour) {
+        smallest = numOne;
+    } else if (numTwo < numOne && numTwo < numThree && numOne < numFour) {
+        smallest = numTwo;
+
+    } else if (numThree < numOne && numThree < numTwo && numOne < numFour) {
+        smallest = numThree;
+    } else {
+        smallest = numFour;
+    }
+
+
+    printf("Largest: %d\n", largest);
+    printf("Smallest: %d\n", smallest);
+
     return 0;
 }
