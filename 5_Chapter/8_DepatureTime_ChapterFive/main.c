bf0d7d2163d992c719f9676ef08f799144f5e4e7
Haider Jan <Haider@DESKTOP-IMIJS5T>
2026-07-17 16:40:07 +0100

    doing project 8 chapter 5

--- 5_Chapter/8_DepatureTime_ChapterFive/main.c
+++ 5_Chapter/8_DepatureTime_ChapterFive/main.c
@@ -1,6 +1,63 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, World!\n");
-    return 0;
+
+    int min;
+    int hour;
+    int time;
+    int com;
+
+    printf("Enter A 24-hour time: ");
+    scanf("%d:%d", &hour, &min);
+
+    time = hour * 60 + min;
+    printf("%d\n",time);
+
+
+    // 8:00 am 480 min
+    // 9:43 am 583 min
+    // 11:19 am 679 min
+    // 12:47 pm 767 min
+    // 2:00 pm 840 min
+    // 3:45 pm 945 min
+    // 7:00 pm 1140 min
+    // 9:45 pm 1305 min
+
+    // 795
+    // 767 or 840
+    // 795 - 767 = 28 12:47 pm
+    // 840 - 795 =  45 2:00 pm
+
+    
+
+
+
+
+
+    if (time < 480) {
+        printf("Closest departure time is 8:00 a.m,. arriving at 10:16 a.m.");
+    } else if (time < 583 ) {
+        printf("Closest departure time is 9:00 a.m,. arriving at 11:52 a.m.");
+    } else if (time < 679 ) {
+        printf("Closest departure time is 11:19 a.m,. arriving at 1:31 p.m.");
+    } else if (time < 767 ) {
+        printf("Closest departure time is 12:47 p.m,. arriving at 3:00 p.m.");
+    } else if (time < 840) {
+        printf("Closest departure time is 2:00 p.m,. arriving at 4:08 p.m.");
+    } else if (time < 1140) {
+        printf("Closest departure time is 7:00 p.m,. arriving at 9:20 p.m.");
+
+    } else {
+        printf("Closest departure time is 9:45 p.m,. arriving at 11:58 p.m.");
+
+    }
+
+ return 0;
+
+
 }
+
+
+
+
+
